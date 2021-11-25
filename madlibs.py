401ba774fe039357ce949b38e107b9901fc44d8f
Chetana16032002 <91176005+Chetana16032002@users.noreply.github.com>
2021-11-25 13:58:08 +0530

    Add files via upload

---
+++ madlibs.py
@@ -0,0 +1,14 @@
+#madlibs 
+# there would be bunch of blanks in the para .fill out those blanks 
+# and then read the paragraph out loud using the blanks that users chose 
+#this project is recreated using the string concatination 
+# youtuber =input ()
+# print("subscribe to " +youtuber )#concatination
+# print(f"subscribe to {youtuber}" )#f string 
+# print("subscribe to {}".format )#format
+adj=input("Adjective:")
+verb=input("verb:")
+verb1=input("verb1:")
+name=input("name:")
+madlib=f"computer programming is so {adj}! it makes me so exited all the time because i love to{verb}.stay happy and stay {verb1} as u are {name}"
+print (madlib)
